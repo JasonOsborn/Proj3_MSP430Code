737462429b52487bb53a08fc6d03011ba728dc60
JasonOsborn <47375292+JasonOsborn@users.noreply.github.com>
2021-11-18 19:43:19 -0600

    Update ProjectActual.ino

--- ProjectActual.ino
+++ ProjectActual.ino
@@ -5,13 +5,13 @@ using namespace std;
 
 LiquidCrystal lcd(P3_3, P6_6, P3_2, P2_7, P4_2, P4_1);
 
-unsigned long globFalseTimer = 15; // The time it takes to send 1 bit. Init value 15 as default/fallthrough.
+long globFalseTimer = 15; // The time it takes to send 1 bit. Init value 15 as default/fallthrough.
 // This value is initialized at 15 milliseconds per bit over 16 bits, but is dynamically changed depending on initial setup phase, as can be seen in StartFlagTRUE functionality
-unsigned long globDelayTracker = 0;
-unsigned long globStorage = 0;
+long globDelayTracker = 0;
+long globStorage = 0;
 int globInputVal = 2;
 
-unsigned long temp = 0;
+long temp = 0;
 int DebugCounter = 0;
 
 int globCounterTemp = 0;
@@ -22,7 +22,7 @@ void setup()
   WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
   P1DIR |= BIT3; ; // Set P1.3 as input -> This is the input from the antenna
 
-  sleep(1);
+  sleep(2);
 
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
@@ -37,7 +37,7 @@ void setup()
   int Counter = 0;
 
   int PrevVal = 0;
-  unsigned long FalseTimerStorage[4] = {};
+  long FalseTimerStorage[4] = {};
   int StartCounter = 0;
 
   while (StartFlag) {
@@ -46,32 +46,33 @@ void setup()
 
     // Manual sync with transmitter. When sync is complete, button is pushed on transmitter side.
     // Super simple edge detection, as setup phase is a 1-0 repeating bitstream.
-    if ((InputVal != PrevVal) && (!StartFlag2)) {
+    if (StartFlag2){
+      StartFlag2 = 0;
+      PrevVal = InputVal;
+    }
+    else if ((InputVal != PrevVal) && (!StartFlag2)) {
       FalseTimerStorage[StartCounter] = millis();
       globStorage = globStorage + InputVal; // add the value to the storage
       PrevVal = InputVal;
       globStorage = globStorage & 0x0F; // remove excess
-      if (++StartCounter > 3) {
-        StartCounter = 0;
-      }
+      StartCounter++;
       if (globStorage == 0b1010) {
         StartFlag = 0;
-        globFalseTimer = ((FalseTimerStorage[3] - FalseTimerStorage[2]) + (FalseTimerStorage[2] - FalseTimerStorage[1]) + (FalseTimerStorage[1] - FalseTimerStorage[0]));
+        globFalseTimer = (FalseTimerStorage[3] - FalseTimerStorage[2]);
+        globFalseTimer = globFalseTimer + (FalseTimerStorage[2] - FalseTimerStorage[1]) ;
+        globFalseTimer = globFalseTimer + (FalseTimerStorage[1] - FalseTimerStorage[0]);
         globFalseTimer = globFalseTimer / 3; // Av. of time between edges
       }
       else {
         globStorage = globStorage << 1; // move the storage over to make room for more val
       }
     }
-    else if ((InputVal != PrevVal) && (StartFlag2)){
-      StartFlag2 = 0;
-      int P_TEMP = P1IN;
-      PrevVal = (P_TEMP & BIT2) >> 2;
-    }
 
   }
   // globFalseTimer should read as the exact value set in the transmitter. If not, a manual reset here is required, and indicates some level of false positive.
   lcd.clear();
+  lcd.print(globStorage,16);
+  lcd.setCursor(0,1);
   lcd.print(globFalseTimer, 16);
   delay(1000);
 
@@ -114,7 +115,7 @@ void loop()
         lcd.print("c");
         polarFlag = 1;
         voltage = (globStorage & 0x7FFF) >> 3;
-        voltage = (voltage + 2.0654) / 1253.8; // Extract message & convert to voltage
+        voltage = voltage / 1247.3; // Extract message & convert to voltage
 
         // Calculate standard deviation of previous 4 + current voltage values
         float mean = (prevVolt[0]+prevVolt[1]+prevVolt[2]+prevVolt[3]+voltage)/5;
@@ -126,19 +127,22 @@ void loop()
         }
 
         // If voltage == 0, it's an incorrect calculation, ignore it. If it is not, save it to prevVolt array.
-        if(voltage != 0)
+        if(voltage > 0.04)
           prevVolt[3] = voltage;
         
         // If current volt is too distant from the mean, display prev. voltage instead of current.
         // ALSO, if voltage reads as 0, display prev. In theory this should also be outside std dev range, but better to set up just in case.
-        // ALSO, if standard dev. is 0, ignore it
-        if((stdDev == 0) && (voltage)){
-          prevVolt[3] = voltage;
-        }
-        else if((abs(voltage-mean)>(2*stdDev)) || (voltage == 0))
+        // ALSO, if standard dev. is 0, ignore it, just push the voltage through (as long as it's not 0)
+        if((stdDev == 0) && (voltage > 0.04)){ }
+        else if((fabs(voltage-mean)>(stdDev/2)) || (voltage < 0.04)){
+          lcd.setCursor(5,1);
+          lcd.print(stdDev,16);
+          lcd.setCursor(10,1);
+          lcd.print(" ");
           lcd.setCursor(11,1);
           lcd.print(voltage,16); // Debug- let us know what the actual value is.
           voltage = prevVolt[2];
+        }
       }
     }
   }
@@ -148,7 +152,9 @@ void loop()
     lcd.print(voltage);
     lcd.print(" Volts");
     lcd.setCursor(0, 1);
-    lcd.print(globCounterTemp++);
+    if(voltage < 0.01){
+      lcd.print("ERR");
+    }
     if(globCounterTemp > 3){
       globCounterTemp = 0;
     }
